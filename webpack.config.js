289703c6728a7d960e09b891ab1363472d785ad5
Leszek Macura <leszek@macura.co>
2018-11-18 20:11:34 +0100

    Zadanie 16_06 dołożony html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+
+
+plugins: [new HtmlWebpackPlugin({
+	template: 'src/index.html',
+	filename: 'index.html',
+	inject: 'body'
+  })]
+    
+
 
 module.exports = {
     entry: './src/index.js',
