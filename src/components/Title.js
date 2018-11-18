3694182066f68b4c5d110861267c3b100f327c9b
Leszek Macura <leszek@macura.co>
2018-11-18 16:30:47 +0100

    Zadanie 16_06 gotowe

---
+++ src/components/Title.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// import style from './Title.css';
+
+const Title = props => <div><h1>{props.title}</h1><h3>liczba zadań: {props.length}</h3></div>;
+	
+
+
+export default Title;
